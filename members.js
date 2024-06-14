16831020a4d588102768d417904bcfff47687331
Ankit kumar <ankit.kumar22@hotmail.com>
2024-06-14 17:44:28 +0000

    copilot second commit

---
+++ members.js
@@ -0,0 +1,30 @@
+function skillMembers() {
+    return {
+        'members': [
+            {
+                'name': 'John Doe',
+                'skills': [
+                    'Javascript',
+                    'Python',
+                    'C++'
+                ]
+            },
+            {
+                'name': 'Jane Doe',
+                'skills': [
+                    'Javascript',
+                    'Python',
+                    'Ruby'
+                ]
+            },
+            {
+                'name': 'Jim Doe',
+                'skills': [
+                    'Java',
+                    'Python',
+                    'Ruby'
+                ]
+            }
+        ]
+    }
+}
